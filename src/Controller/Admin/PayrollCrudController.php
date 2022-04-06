aba9b3d5c228069b1fd8a094c25633e9615e6878
root <hsakib8685@gmail.com>
2022-04-06 15:45:14 +0600

    employee.getSalary at admin

--- src/Controller/Admin/PayrollCrudController.php
+++ src/Controller/Admin/PayrollCrudController.php
@@ -3,23 +3,33 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Payroll;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PayrollCrudController extends AbstractCrudController
 {
+
     public static function getEntityFqcn(): string
     {
         return Payroll::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('employee');
+        yield NumberField::new('month');
+        yield NumberField::new('year');
+        if (Crud::PAGE_EDIT === $pageName|| Crud::PAGE_NEW !== $pageName) {
+            yield TextField::new('employee.getSalary');
+        }
+        yield ChoiceField::new('status')->setChoices([
+            'Generated' => 'false',
+            'Approved' => 'true',
+        ]);
     }
-    */
 }
