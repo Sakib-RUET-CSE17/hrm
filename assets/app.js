ed4d392b4498e726d1a5403fd7e13abc4fa07160
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-06-09 23:49:10 +0600

    firebase

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,66 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// import './styles/app.scss';
+// import 'bootstrap';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 
-// start the Stimulus application
+// // start the Stimulus application
 import './bootstrap';
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyATurGmCsKJ3HvxDoEaeQOMguHUPoJicII",
+    authDomain: "hrm-public-cef61.firebaseapp.com",
+    projectId: "hrm-public-cef61",
+    storageBucket: "hrm-public-cef61.appspot.com",
+    messagingSenderId: "302747101376",
+    appId: "1:302747101376:web:2609a6b61b10ded8d2e525"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// FirebaseUI config.
+var uiConfig = {
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    },
+
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult) {
+            firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function (idToken) {
+                console.log(authResult);
+                // thnew Error('error getting')
+                // Send token to your backend via HTTPS
+                location.replace(`/firebase_phone_auth/${idToken}/${authResult.user.email}`);
+            }).catch(function (error) {
+                console.log(error.message);
+            });
+            return false;
+        }
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth(app));
+// The start method will wait until the DOM is loaded.
+if (document.getElementById('firebaseui-auth-container')) {
+    console.log('paise')
+    ui.start('#firebaseui-auth-container', uiConfig);
+}
---
+++ config/auth/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcoTuVkB14TGCiIzq_gUJCcB2fdzH6nJM",
+    authDomain: "mcmstudio-live.firebaseapp.com",
+    projectId: "mcmstudio-live",
+    storageBucket: "mcmstudio-live.appspot.com",
+    messagingSenderId: "1097705597753",
+    appId: "1:1097705597753:web:78857372ece9d026d416e8"
+};
+
+export default firebaseConfig;
