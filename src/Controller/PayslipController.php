0c8cfcd6341bd064e0831da9fe876aa2525598d9
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-07-06 10:56:36 +0600

    queryBuilder unsuccess

--- src/Controller/PayslipController.php
+++ src/Controller/PayslipController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Admin;
-use App\Repository\PayrollRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PayslipController extends AbstractController
 {
+
+
     #[IsGranted('ROLE_EMPLOYEE')]
     #[Route('/payslip', name: 'app_payslip')]
-    public function index(
-        PayrollRepository $payrollRepository
-    ): Response {
+    public function index(): Response
+    {
         /** @var Admin */
         $user = $this->getUser();
         $employee = $user->getEmployee();
         $payrolls = $employee->getPayrolls();
+        $payrollsSorted = $payrolls->toArray();
+
+        usort($payrollsSorted, function ($payroll1, $payroll2) {
+            // dd($payroll1, $payroll2);
+            if ($payroll1->getWeek() != null) {
+                return $payroll1->getWeek() > $payroll2->getWeek();
+            }
+            return $payroll1->getMonth() > $payroll2->getMonth();
+        });
 
         return $this->render('payslip/index.html.twig', [
             'payrolls' => $payrollsSorted,
