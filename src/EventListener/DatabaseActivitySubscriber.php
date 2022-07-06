5e6f12b9a3d8f4067450d6e89f6cbe921f1a1697
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-07-06 12:10:51 +0600

    log

---
+++ src/EventListener/DatabaseActivitySubscriber.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Log\LogAware;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class DatabaseActivitySubscriber implements EventSubscriberInterface
+{
+    private $tokenStorage;
+    private $requestStack;
+    private $auditLogger;
+
+    public function __construct(TokenStorageInterface $tokenStorage, RequestStack $requestStack, LoggerInterface $auditLogger)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->requestStack = $requestStack;
+        $this->auditLogger = $auditLogger;
+    }
+    // this method can only return the event names; you cannot define a
+    // custom method name to execute when each event triggers
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+            Events::preRemove,
+            Events::preUpdate,
+        ];
+    }
+
+    // callback methods must be called exactly like the events they listen to;
+    // they receive an argument of type LifecycleEventArgs, which gives you access
+    // to both the entity object of the event and the entity manager itself
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $this->logActivity('persist', $args);
+    }
+
+    public function preRemove(LifecycleEventArgs $args): void
+    {
+        $this->logActivity('remove', $args);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args): void
+    {
+        $this->logActivity('update', $args);
+    }
+
+    private function logActivity(string $action, LifecycleEventArgs|PreUpdateEventArgs $args): void
+    {
+        $entity = $args->getObject();
+        // $entityManager = $args->getObjectManager();
+
+        // if this subscriber only applies to certain entity types,
+        // add some code to check the entity type as early as possible
+        // dd($entity);
+        // if (!$entity instanceof LogAware) {
+        // dd($entity);
+        //     return;
+        // }
+
+        $user = $this->tokenStorage->getToken() ? $this->tokenStorage->getToken()->getUser() : null;
+        $date = date('d/m/Y H:i:s');
+
+        $logMessage = ' User ' . $user . ' '
+            . $action . ' '
+            . get_class($entity) . ' information'
+            . ' of id: ' . $entity->getId();
+
+        $context = [
+            'user' => $user,
+            'action' => $action,
+            'entity' => $entity,
+            'id' => $entity->getId(),
+        ];
+
+        if ($action == 'update') {
+            $logMessage .= ' [';
+            $changedFields = $args->getEntityChangeSet();
+            $context['fields'] = $changedFields;
+            foreach ($changedFields as $field => $changes) {
+                $logMessage .= ' ' . $field . '(' . $changes[0] . ' > ' . $changes[1] . ')';
+            }
+            $logMessage .= ' ]';
+        }
+
+        $logMessage .= ' from IP: ' . $this->requestStack->getMainRequest()->getClientIp()
+            . "\n";
+
+        $context['ip'] = $this->requestStack->getMainRequest()->getClientIp();
+
+        // $this->auditLogger->info($logMessage, $context);
+
+        $logsFile = 'audit.log';
+        file_put_contents($logsFile, $logMessage, FILE_APPEND);
+        // dd($logMessage, $this->requestStack->getMainRequest()->getClientIp());
+    }
+}
---
+++ src/Log/LogAware.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Log;
+
+interface LogAware
+{
+}
